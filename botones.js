d17bb79f0a6ad1e294f5bea66121a2b4efdc5e1b
Davidsmait <deivod_halo@hotmail.com>
2019-03-27 22:35:53 -0600

    se agrego el escalamiento con scale3d()

--- botones.js
+++ botones.js
@@ -1,3 +1,4 @@
+// rotar
 let noroeste = document.getElementById('noroeste')
 let noreste = document.getElementById('noreste')
 let suroeste = document.getElementById('suroeste')
@@ -8,27 +9,46 @@ let cubo = document.getElementById('cubo')
 let ejeX = 0
 let ejeY = 0
 
+let escalamiento = 1
+
 noroeste.addEventListener('click', ()=>{
     ejeX+= 10
     ejeY-= 10
-    cubo.style.transform = 'perspective(700px) rotateX(  '+ejeX+'deg ) rotateY(  '+ejeY+'deg )'
+    cubo.style.transform = 'perspective(700px) rotateX(  '+ejeX+'deg ) rotateY(  '+ejeY+'deg ) scale3d('+escalamiento+','+escalamiento+','+escalamiento+')'
     
 })
 
 noreste.addEventListener('click', ()=> {
     ejeX+= 10
     ejeY+= 10
-    cubo.style.transform = 'perspective(700px) rotateX(  '+ejeX+'deg ) rotateY(  '+ejeY+'deg )'
+    cubo.style.transform = 'perspective(700px) rotateX(  '+ejeX+'deg ) rotateY(  '+ejeY+'deg ) scale3d('+escalamiento+','+escalamiento+','+escalamiento+')'
 })
 
 suroeste.addEventListener('click', ()=> {
     ejeX-= 10
     ejeY-= 10
-    cubo.style.transform = 'perspective(700px) rotateX(  '+ejeX+'deg ) rotateY(  '+ejeY+'deg )'
+    cubo.style.transform = 'perspective(700px) rotateX(  '+ejeX+'deg ) rotateY(  '+ejeY+'deg ) scale3d('+escalamiento+','+escalamiento+','+escalamiento+')'
 })
 
 sureste.addEventListener('click', ()=> {
     ejeX-= 10
     ejeY+= 10
-    cubo.style.transform = 'perspective(700px) rotateX(  '+ejeX+'deg ) rotateY(  '+ejeY+'deg )'
+    cubo.style.transform = 'perspective(700px) rotateX(  '+ejeX+'deg ) rotateY(  '+ejeY+'deg ) scale3d('+escalamiento+','+escalamiento+','+escalamiento+')'
+})
+
+// reducir
+
+let reducir = document.getElementById('reducir')
+let aumentar = document.getElementById('aumentar')
+
+
+
+reducir.addEventListener('click', ()=> {
+    escalamiento-= .10
+    cubo.style.transform = 'perspective(700px) rotateX(  '+ejeX+'deg ) rotateY(  '+ejeY+'deg ) scale3d('+escalamiento+','+escalamiento+','+escalamiento+')'
+})
+
+aumentar.addEventListener('click', ()=>{
+    escalamiento+= .10
+    cubo.style.transform = 'perspective(700px) rotateX(  '+ejeX+'deg ) rotateY(  '+ejeY+'deg ) scale3d('+escalamiento+','+escalamiento+','+escalamiento+')'
 })
